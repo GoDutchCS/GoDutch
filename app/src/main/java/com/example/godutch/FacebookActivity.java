85ff7c665c799816205a1d903222225bc51be88e
pacokwon <haechank@gmail.com>
2020-07-17 04:10:01 +0900

    add: okhttp3 dependency and template code for post request

--- app/src/main/java/com/example/godutch/FacebookActivity.java
+++ app/src/main/java/com/example/godutch/FacebookActivity.java
@@ -2,7 +2,6 @@ package com.example.godutch;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,8 +11,22 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class FacebookActivity extends AppCompatActivity {
-    CallbackManager callbackManager;
+    private CallbackManager callbackManager;
+    private OkHttpClient client = new OkHttpClient();
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +35,32 @@ public class FacebookActivity extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
         loginButton.setReadPermissions("email");
+        loginButton.setReadPermissions("email");
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                // App code
+                String postBody = "{\n" +
+                        "\"access_token\": \"" + loginResult.getAccessToken().getToken() + "\"\n" +
+                        "}";
+                RequestBody body = RequestBody.create(postBody, JSON);
+                Request request = new Request.Builder()
+                        .url(String.format("%s/auth/login", Constants.SERVER_IP))
+                        .post(body)
+                        .build();
+
+
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        call.cancel();
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        final String data = response.body().string();
+                    }
+                });
             }
             @Override
             public void onCancel() {
