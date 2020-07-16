36bbb94bfd5784e0351de377275a7b8ff2196a36
pacokwon <haechank@gmail.com>
2020-07-16 19:35:19 +0900

    update: rename default Notifications to Gallery

--- app/src/main/java/com/example/godutch/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/godutch/ui/gallery/GalleryFragment.java
@@ -1,4 +1,4 @@
-package com.example.godutch.ui.notifications;
+package com.example.godutch.ui.gallery;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -14,17 +14,17 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.godutch.R;
 
-public class NotificationsFragment extends Fragment {
+public class GalleryFragment extends Fragment {
 
-    private NotificationsViewModel notificationsViewModel;
+    private GalleryViewModel galleryViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        galleryViewModel =
+                ViewModelProviders.of(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_notifications, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
-        notificationsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
