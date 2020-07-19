9a3fa1541b743aaade79d3af2ddda8548b41fadf
pacokwon <haechank@gmail.com>
2020-07-19 14:02:18 +0900

    add: hide actionbar on fragment resume

--- app/src/main/java/com/example/godutch/ui/godutch/GoDutchFragment.java
+++ app/src/main/java/com/example/godutch/ui/godutch/GoDutchFragment.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -31,5 +32,17 @@ public class GoDutchFragment extends Fragment {
             }
         });
         return root;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
     }
 }
