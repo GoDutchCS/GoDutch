a4601341bf47ebb39a969a45f73bd84548c28d5a
pacokwon <haechank@gmail.com>
2020-07-20 13:47:13 +0900

    add: auto transition to next fragment on party creation

--- app/src/main/java/com/example/godutch/ui/godutch/party/GoDutchPartyFragment.java
+++ app/src/main/java/com/example/godutch/ui/godutch/party/GoDutchPartyFragment.java
@@ -1,6 +1,9 @@
 package com.example.godutch.ui.godutch.party;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,9 +13,26 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.godutch.Constants;
+import com.example.godutch.MainActivity;
 import com.example.godutch.R;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class GoDutchPartyFragment extends Fragment {
+    private static int REQUEST_PARTY_DETAIL_ACTIVITY = 1;
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private OkHttpClient client = new OkHttpClient();
     private RecyclerView parties;
     private GoDutchPartyAdapter adapter;
     private String userID;
@@ -29,5 +49,36 @@ public class GoDutchPartyFragment extends Fragment {
         parties.setAdapter(adapter);
 
         return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_PARTY_DETAIL_ACTIVITY && resultCode == Activity.RESULT_OK)  {
+            String postBody = data.getStringExtra("party");
+            Log.v("Foo", postBody);
+            RequestBody body = RequestBody.create(postBody, JSON);
+            Request request = new Request.Builder()
+                    .url(String.format("%s/api/parties/add", Constants.SERVER_IP))
+                    .post(body)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                    call.cancel();
+                }
+
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                    final String jsonString = response.body().string();
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            ((MainActivity)getActivity()).getViewPager().setCurrentItem(1);
+                        }
+                    });
+                }
+            });
+        }
     }
 }
